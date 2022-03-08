205fe17f76bd9fb463470d3078ce885925b12e26
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2022-03-07 22:16:19 -0300

    Initial Commit

---
+++ Shared/MyEasyCryptoManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEasyCryptoManagerApp.swift
+//  Shared
+//
+//  Created by Jacob Aguilar on 07-03-22.
+//
+
+import SwiftUI
+
+@main
+struct MyEasyCryptoManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
