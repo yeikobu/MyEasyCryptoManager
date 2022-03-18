66cf4d2194f615efa5dd9e23405aadf8fbd7780f
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2022-03-18 19:30:45 -0300

    Added haptics to refreshable scroll view

--- Shared/View/CoinsListView.swift
+++ Shared/View/CoinsListView.swift
@@ -28,6 +28,7 @@ struct CoinsMarketListView: View {
     
     @ObservedObject var coinViewModel: CoinInfoViewModel = CoinInfoViewModel()
     let gridForm = [GridItem(.flexible())]
+    @State var engine: CHHapticEngine?
     
     var body: some View {
         VStack {
@@ -103,18 +104,60 @@ struct CoinsMarketListView: View {
                             CoinsDataListView(name: coin.name ?? "", marketCapRank: coin.marketCapRank ?? 0, symbol: coin.symbol ?? "", priceChangePercentage: coin.priceChangePercentage24H ?? 0, currentPrice: coin.currentPrice ?? 0, marketCap: coin.marketCap ?? 0, imgURL: coin.image ?? "", totalVolume: coin.totalVolume ?? 0, high24H: coin.high24H ?? 0, low24H: coin.low24H ?? 0, maxSupply: coin.maxSupply ?? 0, totalSupply: coin.totalSupply ?? 0, circulatingSupply: coin.circulatingSupply ?? 0, ath: coin.ath ?? 0, atl: coin.atl ?? 0, isTouched: false, isListVisible: false)
                         }
                     }
-                   
                 }
                 .cornerRadius(10)
                 .padding(.horizontal, 10)
                 .ignoresSafeArea()
                 .refreshable {
+                    complexSuccess()
                     try? await Task.sleep(nanoseconds: 1000000000)
                     coinViewModel.updateInfo()
                 }
                 
                 
             }
+        }
+        .onAppear {
+            prepareHaptics()
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return } //If divice not support haptics
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        for i in stride(from: 0, to: 0.5, by: 0.09) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+            events.append(event)
+        }
+        
+//        for i in stride(from: 0.4, to: 0.5, by: 0.15) {
+//            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+//            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+//            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0.25)
+//            events.append(event)
+//        }
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print(error.localizedDescription)
         }
     }
 }
@@ -593,22 +636,18 @@ struct CoinsDataListView: View {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         
         var events = [CHHapticEvent]()
-//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(4))
-//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(5))
-//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-//        events.append(event)
         
-        for i in stride(from: 0, to: 1, by: 0.09) {
+        for i in stride(from: 0, to: 0.5, by: 0.09) {
             let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
             let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
             let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
             events.append(event)
         }
         
-        for i in stride(from: 0.45, to: 0.5, by: 0.15) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+        for i in stride(from: 0.4, to: 0.5, by: 0.15) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1.2 - i))
             let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0.1)
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0.25)
             events.append(event)
         }
         
