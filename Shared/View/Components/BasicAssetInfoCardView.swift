969785fc4b89a5337280d75046e57b52a77627e9
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2022-03-27 15:17:37 -0300

    modiefied

--- Shared/View/Components/BasicAssetInfoCardView.swift
+++ Shared/View/Components/BasicAssetInfoCardView.swift
@@ -150,10 +150,7 @@ struct BasicAssetInfoCardView: View {
                 .blur(radius: 0)
                 .opacity(0.9)
                 .matchedGeometryEffect(id: "background", in: animation)
-
         )
-        .matchedGeometryEffect(id: "background", in: animation)
-        
         .mask(
             RoundedRectangle(cornerRadius: 10, style: .continuous)
                 .matchedGeometryEffect(id: "mask", in: animation)
@@ -171,6 +168,5 @@ struct BasicAssetInfoCardView_Previews: PreviewProvider {
     
     static var previews: some View {
         BasicAssetInfoCardView(name: $name, marketCapRank: $marketCapRank, symbol: $name, priceChangePercentage: $priceChangePercentage, currentPrice: $priceChangePercentage, marketCap: $marketCapRank, imgURL: $name, totalVolume: $priceChangePercentage, high24H: $priceChangePercentage, low24H: $priceChangePercentage, maxSupply: $priceChangePercentage, totalSupply: $priceChangePercentage, circulatingSupply: $priceChangePercentage, ath: $priceChangePercentage, atl: $priceChangePercentage, isTouched: .constant(true), isListVisible: .constant(true), animation: animation)
-            .preferredColorScheme(.dark)
     }
 }
